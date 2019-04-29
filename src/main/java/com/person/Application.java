8815663caa2a10ef34cfd7381f9e84a75a61e25e
aabdellatif <aabdellatif@netseenergy.fr>
2019-04-29 15:22:33 +0200

    add spring boot

--- src/main/java/com/person/Application.java
+++ src/main/java/com/person/Application.java
@@ -1,12 +1,23 @@
 package com.person;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	JobLauncher jobLauncher;
+
+	@Autowired
+	Job job;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -16,4 +27,14 @@ public class Application {
 	public String home() {
 		return "Hello Docker worldddd for Nadia";
 	}
+
+	@Override
+	public void run(String... args) throws Exception
+	{
+		JobParameters params = new JobParametersBuilder()
+				.addString("JobID", String.valueOf(System.currentTimeMillis()))
+				.toJobParameters();
+		jobLauncher.run(job, params);
+	}
+
 }
